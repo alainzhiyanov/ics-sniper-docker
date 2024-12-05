f465d852648205eb0fa8007782ce16fa64ea1686
alainzhiyanov <58162448+alainzhiyanov@users.noreply.github.com>
2024-12-05 13:42:47 -0800

    Create ENTRYPOINT.sh

---
+++ docker-for-py2-ics-sniper/ENTRYPOINT.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+service openvswitch-switch start
+ovs-vsctl set-manager ptcp:6640
+pip2 install pandas minicps
+
+if [ $# -gt 0 ]
+then
+  if [ "$1" == "mn" ]
+  then
+    bash -c "$@"
+  else
+    mn "$@"
+  fi
+else
+  bash
+fi
+
+service openvswitch-switch stop
